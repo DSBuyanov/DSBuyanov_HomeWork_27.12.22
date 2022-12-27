7e4643c76f1cb61a32d90f5051b2c497d7f6c660
DSBuyanov <inquis87@gmail.com>
2022-12-27 10:44:07 +0300

    2.5

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15-> 120
+// M = 4; N = 8. -> 30
+
+int Sum(int a, int b, int sum)
+{
+    if (a <= b){
+        sum = sum + a;
+        a++;
+        return Sum(a, b, sum);
+    }
+    return sum; 
+} 
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+int sum = 0;
+Console.WriteLine($"Число равно {Sum(a, b, sum)}");
